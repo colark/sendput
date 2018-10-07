ae8b554a7926e56cd741a7208ba2dd04a01c063f
Lian <thompsonlian@yahoo.com>
2018-10-07 02:01:48 -0700

    email already in db

--- backend/manage/index.js
+++ backend/manage/index.js
@@ -1,9 +1,10 @@
 import bodyParser from 'body-parser';
-
+const sgMail = require('@sendgrid/mail');
 const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
-
 const { Prisma } = require('prisma-binding');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default {
   withServer: (app, { context }) => {
@@ -18,7 +19,31 @@ export default {
       const userExists = await db.exists.User({
         email: req.query.email
       });
-      res.send(`hi! user exists? ${userExists}`);
+      res.status(200);
+      if(!userExists){
+        const user = await db.mutation
+              .createUser(
+                  {
+                      data: {
+                          email: req.query.email,
+                      }
+                  }
+              )
+              .catch((err) => {
+                  console.error(err);
+                  throw new Error("Server error: couldn't perform signUp mutation.");
+              });
+        const msg = {
+          to: 'lianythompson@gmail.com',
+          from: 'lian@colark.com',
+          subject: 'HI LIAN',
+          text: 'and easy to do anywhere, even with Node.js',
+          html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+        }
+        sgMail.send(msg);
+        res.send(`Hi! we sent you an email ${userExists}`);
+      }
+      res.send('Hi, no email.');
       res.end();
     })
 
